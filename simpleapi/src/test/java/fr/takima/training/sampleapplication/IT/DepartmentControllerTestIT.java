d065aff676b81956092bc879313001268f4a6606
iamjuli3n-cmd <julienblondiaux78400@gmail.com>
2026-06-02 10:59:45 +0200

    modifs

--- simpleapi/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ simpleapi/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(classes = {SimpleApiApplication.class})
 class DepartmentControllerTestIT {
--- simpleapi/src/test/java/fr/takima/training/sampleapplication/IT/StudentControllerTestIT.java
+++ simpleapi/src/test/java/fr/takima/training/sampleapplication/IT/StudentControllerTestIT.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +14,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(classes = {SimpleApiApplication.class})
 class StudentControllerTestIT {
